c6775d450d03633761d8a0dd02ceae5f610ba4a3
Maria Eduarda Marques <Dudasme2445@hotmail.com>
2025-06-16 01:46:58 -0300

    Classe Curso

--- Strategy_Curso/Models/Curso.cs
+++ Strategy_Curso/Models/Curso.cs
@@ -2,5 +2,39 @@
 {
     public class Curso
     {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public int cargaHoraria { get; set; }
+        public string descricao { get; set; }
+        public PublicoAlvoEnum publicoAlvo { get; set; }
+        public double valor { get; set; }
+
+
+
+        public Curso() { }
+
+        public Curso(int id, string nome, int cargaHoraria, PublicoAlvoEnum publicoAlvo, double valor, string descricao)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.cargaHoraria = cargaHoraria;
+            this.publicoAlvo = publicoAlvo;
+            this.valor = valor;
+            this.descricao = descricao;
+        }
+
+        public override string ToString()
+        {
+            return $"Curso: {nome}, Carga Horária: {cargaHoraria} horas, Público Alvo: {publicoAlvo}, Valor: R${valor}, Descrição: {descricao}";
+        }
     }
+         public enum PublicoAlvoEnum
+    {
+        Infantil,
+        Adolescente,
+        Adulto,
+        JovemAdulto,
+        Todos
+    }
+
 }
