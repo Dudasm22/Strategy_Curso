ae91604228cc8233f2c60ae6b7b34b79559e056b
Maria Eduarda Marques <Dudasme2445@hotmail.com>
2025-06-16 02:05:47 -0300

    Strategy

---
+++ Strategy_Curso/Strategy/MySQLStrategy.cs
@@ -0,0 +1,31 @@
+﻿using Strategy_Curso.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace Strategy_Curso.Strategy
+{
+    public class MySQLStrategy : IDataBaseStrategy
+    {
+        private static List<Curso> cursos = new List<Curso>();
+        public static int nextId = 1;
+
+        public void InserirCurso(Curso curso)
+        {
+            curso.id = nextId++;
+            cursos.Add(curso);
+            Console.WriteLine($"[MySQL] Curso '{curso.nome}' inserido com sucesso!");
+        }
+        public List<Curso> VisualizarCursos()
+        {
+            Console.WriteLine("[MySQL] Listando cursos:");
+            if (cursos.Count == 0)
+            {
+                Console.WriteLine("Nenhum curso cadastrado.");
+               // return new List<Curso>();
+            }
+            return cursos;
+
+        }
+    }
+}
